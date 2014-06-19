5722c30e43988f0f4d82ea11000d556faca76c30
Shadow26 <gor26.91@mail.ru>
2014-06-19 09:04:36 +0300

    Create RateUsdUah.java

---
+++ src/RateUsdUah.java
@@ -0,0 +1,60 @@
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RateUsdUah {
+
+	public static final Pattern pattern = Pattern
+			.compile("yfs_l10_usduah=x\">[0-9]{2}\\.[0-9]{2,10}<");
+
+	public static void doMatch(String content) {
+
+		Matcher matcher = pattern.matcher(content);
+		while (matcher.find()) {
+
+			String str = (matcher.group()).toString();
+			String rateUsdUah = str.substring((str.length() - 8),
+					(str.length() - 1));
+			System.out.println("USD/UAH = " + Float.parseFloat(rateUsdUah));
+		}
+	}
+
+	private static void copy(InputStream in, OutputStream out)
+			throws IOException {
+		byte[] buffer = new byte[1024];
+		while (true) {
+			int readCount = in.read(buffer);
+			if (readCount == -1) {
+				break;
+			}
+			out.write(buffer, 0, readCount);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		URL url = new URL("http://finance.yahoo.com/q?s=USDUAH=X");
+		InputStream in = url.openStream();
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try {
+
+			ObjectOutputStream oos = new ObjectOutputStream(baos);
+			try {
+				copy(in, oos);
+			} finally {
+				oos.close();
+			}
+		} finally {
+			in.close();
+		}
+		String str = baos.toString();
+		doMatch(str);
+	}
+}

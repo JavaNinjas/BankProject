9bae36f95b42658f1755c77bf5772345f92777d8
Shadow26 <gor26.91@mail.ru>
2014-07-30 11:55:25 +0300

    Update RateEurUsd.java

--- src/main/java/parser/RateEurUsd.java
+++ src/main/java/parser/RateEurUsd.java
@@ -4,27 +4,25 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RateEurUsd {
-	private static String finalresult;
+    private static String finalresult;
 
-	public String getFinalresult() {
-		return finalresult;
-	}
+    public String getFinalresult() {
+        return finalresult;
+    }
 
-	public void setFinalresult(String result) {
-		RateEurUsd.finalresult = result;
-	}
+    public void setFinalresult(String result) {
+        RateEurUsd.finalresult = result;
+    }
 
-	public static final Pattern pattern = Pattern
-			.compile("yfs_l10_eurusd=x\">[0-9]\\.[0-9]{2,10}<");
+    public static final Pattern pattern = Pattern
+            .compile("yfs_l10_eurusd=x\">[0-9]\\.[0-9]{2,10}<");
 
-	public void doMatch(String content) {
-		String rateEurUsd = "";
-		Matcher matcher = pattern.matcher(content);
-		while (matcher.find()) {
+    public void findRate(String content) {
+        Matcher matcher = pattern.matcher(content);
+        while (matcher.find()) {
+            String str = (matcher.group()).toString();
+            setFinalresult(str.substring((str.length() - 7), (str.length() - 1)));
+        }
 
-			String str = (matcher.group()).toString();
-			rateEurUsd = str.substring((str.length() - 7), (str.length() - 1));
-		}
-		setFinalresult(rateEurUsd);
-	}
+    }
 }
